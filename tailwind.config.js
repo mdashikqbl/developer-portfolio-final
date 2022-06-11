830f814e9572d2ce6592e4067e2b35b137efab28
mdashikqbl <mdashikqbl@gmail.com>
2022-06-11 06:03:21 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        devmytheme: {
+          primary: "#185ADB",
+          secondary: "#F15412",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
